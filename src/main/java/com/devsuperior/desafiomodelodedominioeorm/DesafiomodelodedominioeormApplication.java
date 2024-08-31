43a3f46bb4e3c2758ca5d6f9b76f7d7ec0129ad8
gustavo-r-nunes <gustavo.r.nunes97@gmail.com>
2024-08-31 20:53:52 -0300

    first commit

---
+++ src/main/java/com/devsuperior/desafiomodelodedominioeorm/DesafiomodelodedominioeormApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodelodedominioeorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiomodelodedominioeormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiomodelodedominioeormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/desafiomodelodedominioeorm/DesafiomodelodedominioeormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodelodedominioeorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiomodelodedominioeormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
